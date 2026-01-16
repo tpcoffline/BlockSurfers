0bca1b9988b805ea09852ff06641bf15e7a9aaf1
tpcoffline <beym75121@gmail.com>
2026-01-16 19:18:25 +0300

    ilk committeki eksik dosyaları pushladım

---
+++ common/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    `java-library`
+}
+
+dependencies {
+    api(Libs.minestom)
+    api(Libs.Adventure.text)
+    api(Libs.Adventure.api)
+    api(Libs.joml)
+    api(Libs.postgres)
+    api(Libs.hikari)
+}
